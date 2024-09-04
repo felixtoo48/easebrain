a5711812634d46ae1841de8c71cdab52219d3f68
felixtoo48 <felixtoo48@gmail.com>
2024-09-04 18:11:53 +0300

    creating url path for landing page

--- easebrainapp/urls.py
+++ easebrainapp/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from easebrain import views as easebrain_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
+    # easebrain URLs
+    path('', easebrain_views.index, name='index'),
+    path('easebrain/', include('easebrain.urls')),
 ]
