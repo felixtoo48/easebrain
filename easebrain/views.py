3d36a86ab4469368fc93abede48c39fb17ab4326
felixtoo48 <felixtoo48@gmail.com>
2024-09-04 18:52:25 +0300

    sign up request creation

--- easebrain/views.py
+++ easebrain/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.views.generic.detail import DetailView
 import os
+
+from .forms import SignUpForm
 
 
 def index(request):
     context = {}
     return render(request, 'easebrain/index.html', context)
+
+
+class UserView(DetailView):
+    template_name = 'users/profile.html'
+
+    def get_object(self):
+        return self.request.user
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(request, email=user.email, password=raw_password)
+            if user is not None:
+                login(request, user)
+            else:
+                print("user is not authenticated")
+            return redirect('users:profile')
+    else:
+        form = SignUpForm()
+    return render(request, 'users/signup.html', {'form': form})
