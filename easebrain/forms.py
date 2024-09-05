e16aae08bd84638560b40b5ae1d4ceb18341a78a
felixtoo48 <felixtoo48@gmail.com>
2024-09-05 16:58:31 +0300

    updating the login forms input

--- easebrain/forms.py
+++ easebrain/forms.py
@@ -2,9 +2,26 @@ from django import forms
 from .models import *
 from django.db import models
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import widgets
+
+#Form Layout from Crispy Forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email',)
+
+class UserLoginForm(forms.ModelForm):
+    username = forms.CharField(
+                            widget=forms.TextInput(attrs={'id': 'floatingInput', 'class': 'form-control mb-3'}),
+                            required=True)
+    password = forms.CharField(
+                            widget=forms.PasswordInput(attrs={'id': 'floatingPassword', 'class': 'form-control mb-3'}),
+                            required=True)
+
+    class Meta:
+        model=User
+        fields=['username','password']
