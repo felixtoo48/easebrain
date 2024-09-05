e905014ebfd62e877ea7e314c4c681058f0baa1a
felixtoo48 <felixtoo48@gmail.com>
2024-09-05 19:57:43 +0300

    imporing signal handler to app.py

--- easebrain/apps.py
+++ easebrain/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class EasebrainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'easebrain'
+
+    def ready(self):
+        # Import signals
+        import easebrain.signals
