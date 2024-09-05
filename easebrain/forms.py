b8eedb7e4938b9a6a3ba9618e835e12beafc4c60
felixtoo48 <felixtoo48@gmail.com>
2024-09-05 17:41:35 +0300

    commenting on unnecessary UserLoginForm addition

--- easebrain/forms.py
+++ easebrain/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from .models import *
 from django.db import models
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import widgets
+# from django.forms import widgets
 
+"""
 #Form Layout from Crispy Forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
+"""
 
 
 class SignUpForm(UserCreationForm):
@@ -14,6 +16,7 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('email',)
 
+"""
 class UserLoginForm(forms.ModelForm):
     username = forms.CharField(
                             widget=forms.TextInput(attrs={'id': 'floatingInput', 'class': 'form-control mb-3'}),
@@ -25,3 +28,4 @@ class UserLoginForm(forms.ModelForm):
     class Meta:
         model=User
         fields=['username','password']
+"""
