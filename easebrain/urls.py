846e262e94fc96040257f944dab866f979e69d2e
felixtoo48 <felixtoo48@gmail.com>
2024-09-07 00:28:27 +0300

    adding update profile url

--- easebrain/urls.py
+++ easebrain/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 
-from .views import UserView, signup
+from .views import UserView, signup, update_profile
 
 app_name = 'easebrain'
 
@@ -12,5 +12,6 @@ urlpatterns = [
         path('login/', auth_views.LoginView.as_view(template_name='easebrain/login.html'), name='login'),
         path('logout/', auth_views.LogoutView.as_view(next_page='/easebrain/login'), name='logout'),
         path('profile/',  login_required(UserView.as_view()), name='profile'),
-        path('signup/', signup, name='signup')
+        path('signup/', signup, name='signup'),
+        path('profile/update/', update_profile, name='update_profile'),
 ]
